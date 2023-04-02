5750b2b2741f7d874b9d04b3219d44a702b05f05
Wayne <yky32is@gmail.com>
2023-04-03 03:39:31 +0800

    Add JWT as resources server

---
+++ src/main/java/com/quinsic/servicename/config/JWTSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.quinsic.servicename.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class JWTSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .authorizeRequests(authz -> authz.antMatchers(HttpMethod.GET, "/foos/**")
+                        .hasAuthority("SCOPE_read")
+                        .antMatchers(HttpMethod.POST, "/foos")
+                        .hasAuthority("SCOPE_write")
+                        .anyRequest()
+                        .authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+                .build();
+    }
+    @Bean
+    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
+        return NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri())
+                .build();
+    }
+}
